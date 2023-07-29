341e5a5d47d8e7a015c2268df557b1a6b8a612e7
asnewman <ashznewman@gmail.com>
2023-07-29 12:07:56 -0700

    query for feed

--- src/feed.ts
+++ src/feed.ts
@@ -7,21 +7,39 @@ const router = express.Router();
 
 const axios = getAxios();
 
-router.get("", async (_, res) => {
-  const response = await axios.get("https://tildes.net", options)
+router.get("", async (req, res) => {
+  const { order, period } = req.query;
+  let params = "";
+
+  if (!order) {
+    params += `?order=activity`;
+  } else {
+    params += `?order=${order}`;
+  }
+
+  if (period) {
+    params += `&period=${period}`;
+  }
+
+  const url = "https://tildes.net" + params;
+
+  const response = await axios.get(url, options);
   const $ = cheerio.load(response.data);
 
-  const feed: FeedTopic[] = []
+  const feed: FeedTopic[] = [];
 
   $("article.topic").each((_, topicElement) => {
-    const group = $(topicElement).find(".link-group").text().replace("~", "")
-    const link = $(topicElement).find("header > a").attr("href")
-    const commentLink = $(topicElement).find(".topic-info-comments > a").attr("href")
-    const id = commentLink.split("/")[2] 
-    const title = $(topicElement).find(".topic-title > a").text()
-    const author = $(topicElement).find(".topic-info-source > a").text()
-    const votes = parseInt($(topicElement).find(".topic-voting-votes").text(), 10) || 0
-    const datePosted = $(topicElement).find("time").attr("datetime")
+    const group = $(topicElement).find(".link-group").text().replace("~", "");
+    const link = $(topicElement).find("header > a").attr("href");
+    const commentLink = $(topicElement)
+      .find(".topic-info-comments > a")
+      .attr("href");
+    const id = commentLink.split("/")[2];
+    const title = $(topicElement).find(".topic-title > a").text();
+    const author = $(topicElement).find(".topic-info-source > a").text();
+    const votes =
+      parseInt($(topicElement).find(".topic-voting-votes").text(), 10) || 0;
+    const datePosted = $(topicElement).find("time").attr("datetime");
 
     feed.push({
       id,
@@ -30,11 +48,11 @@ router.get("", async (_, res) => {
       title,
       author,
       votes,
-      datePosted
-    })
-  })
+      datePosted,
+    });
+  });
 
-  res.json(feed)
-})
+  res.json(feed);
+});
 
 export default router;
